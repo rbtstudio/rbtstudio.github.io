15a9db19488aaa5cde02479e8d669bd04fadc9b2
Matthew Higley <rbtstudio@gmail.com>
2014-04-09 09:36:17 -0700

    php test

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Bootcamp2 Examples</title>
+	<link rel="stylesheet" type="text/css" href="/_/css/styles.css">
+</head>
+<body>
+
+	<header>
+	<nav>
+		<ul>
+			<li><a href="#">link one</a></li>
+			<li><a href="#">link two</a></li>
+			<li><a href="#">link three</a></li>
+			<li><a href="#">link four</a></li>
+		</ul>
+	</nav>
+	</header>
+
+	<main>
+		
+	</main>
+
+	<footer>
+		
+	</footer>
+
+</body>
+</html>
